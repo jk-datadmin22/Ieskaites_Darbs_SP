e544be5879a16dc5220b7b043aabf1a5d6d0035d
SandisPAX <sandispax@inbox.lv>
2022-07-12 15:59:44 +0300

    Izveidots melnraksts

--- Superbingo/Superbingo.py
+++ Superbingo/Superbingo.py
@@ -3,7 +3,7 @@ import time
 from bs4 import BeautifulSoup as bs
 import csv
 
-# print("=================================")
+
 
 ADRESE = "https://www.latloto.lv/lv/rezultati/superbingo/1147512"
 
@@ -20,51 +20,4 @@ def saglabat_lapu(adrese, fails):
         print("radās kļūda", pieprasijums.status_code)
         return
 
-saglabat_lapu(ADRESE, "Superbingo/Lapas/lapa_10.html") 
-
-# print("=================================")
-
-# import requests
-# import time
-
-# ADRESE = "https://www.latloto.lv/lv/rezultati/superbingo/"
-
-# def atvilkt_lapas(daudzums):
-#     for i in range(1147530, daudzums - 2):
-#         adrese = f"{ADRESE}page{i}.html"
-#         fails = f"Superbingo/lapas/lapa_{i}.html"
-
-#         print("Pieprasijums uz", adrese, "--->.",  fails)
-#         saglabat_lapu(adrese, fails)
-
-#         time.sleep(2)
-
-# atvilkt_lapas(10)
-
-# print("=================================")
-
-def info(htmlDatne):
-    with open(htmlDatne, "r", encoding="utf-8") as fails:
-        html = fails.read()
-
-    zupa = bs(html, "html.parser")
-
-    tabulas = zupa.find_all("numbered-items numbered-items-purple margin-bottom-5")
- 
-    for tabula in tabulas:
-        print (tabula)
-        print("=================================")
-        print("=================================")
-        print("=================================")
-        print("=================================")
-        print("=================================")
-        print("=================================")
-
-    print(zupa)
-
-#     tabulas = zupa.find
-
-        # print(html)
-
-
-info("Superbingo/Lapas/lapa_10.html") 
+saglabat_lapu(ADRESE, "Superbingo/Lapas/lapa_10.html")
