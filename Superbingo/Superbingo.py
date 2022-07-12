363de012f4540e6a562d47bcbdbc47e7106169c0
SandisPAX <sandispax@inbox.lv>
2022-07-12 14:13:08 +0300

    nestrādā tabula

--- Superbingo/Superbingo.py
+++ Superbingo/Superbingo.py
@@ -1,5 +1,9 @@
 import requests
 import time
+from bs4 import BeautifulSoup as bs
+import csv
+
+# print("=================================")
 
 ADRESE = "https://www.latloto.lv/lv/rezultati/superbingo/1147512"
 
@@ -18,12 +22,15 @@ def saglabat_lapu(adrese, fails):
 
 saglabat_lapu(ADRESE, "Superbingo/Lapas/lapa_10.html") 
 
-# eeeeeeeeee
+# print("=================================")
+
+# import requests
+# import time
 
 # ADRESE = "https://www.latloto.lv/lv/rezultati/superbingo/"
 
 # def atvilkt_lapas(daudzums):
-#     for i in range(1, daudzums - 2):
+#     for i in range(1147530, daudzums - 2):
 #         adrese = f"{ADRESE}page{i}.html"
 #         fails = f"Superbingo/lapas/lapa_{i}.html"
 
@@ -34,24 +41,30 @@ saglabat_lapu(ADRESE, "Superbingo/Lapas/lapa_10.html")
 
 # atvilkt_lapas(10)
 
-# def info(htmlDatne):
-#     with open(htmlDatne, "r", encoding="utf-8") as fails:
-#         html = fails.read()
+# print("=================================")
 
-#         print(html)
+def info(htmlDatne):
+    with open(htmlDatne, "r", encoding="utf-8") as fails:
+        html = fails.read()
 
-# def info(htmlDatne):
-#     with open(htmlDatne, "r", encoding="utf-8") as fails:
-#         html = fails.read()
+    zupa = bs(html, "html.parser")
 
-#     zupa = bs(html, "html.parser")
+    tabulas = zupa.find_all("numbered-items numbered-items-purple margin-bottom-5")
+ 
+    for tabula in tabulas:
+        print (tabula)
+        print("=================================")
+        print("=================================")
+        print("=================================")
+        print("=================================")
+        print("=================================")
+        print("=================================")
+
+    print(zupa)
 
 #     tabulas = zupa.find
 
+        # print(html)
 
 
-#         print(html)
-
-
-
-# info("7diena/requests/lapas/lapa_1.html") 
+info("Superbingo/Lapas/lapa_10.html") 
